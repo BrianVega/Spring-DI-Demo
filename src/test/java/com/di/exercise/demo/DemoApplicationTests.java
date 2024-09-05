c474b778a9893ff7d2cbc20481d03f21506a1502
Brian Vega <BrianVega01@outlook.es>
2024-09-05 11:24:37 -0600

    Spring Test Context

--- src/test/java/com/di/exercise/demo/DemoApplicationTests.java
+++ src/test/java/com/di/exercise/demo/DemoApplicationTests.java
@@ -1,10 +1,31 @@
 package com.di.exercise.demo;
 
+import com.di.exercise.demo.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+	@Autowired // Using application Context
+	ApplicationContext applicationContext;
+
+	@Autowired // Using Bean
+	MyController myController;
+
+	@Test
+	void testGetControllerFromContext() {
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromMyController() {
+		System.out.println(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
