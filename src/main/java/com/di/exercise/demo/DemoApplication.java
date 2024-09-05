707d6899402dbdead5fd92b129cd1e2fb211d03d
Brian Vega <BrianVega01@outlook.es>
2024-09-05 11:16:46 -0600

    The Spring Context

--- src/main/java/com/di/exercise/demo/DemoApplication.java
+++ src/main/java/com/di/exercise/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package com.di.exercise.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+
+		MyController controller = applicationContext.getBean(MyController.class);
+
+		System.out.println("In main method");
+		System.out.println(controller.sayHello());
 	}
 
 }
